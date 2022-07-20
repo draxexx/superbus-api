ca5659c49bcfc716b017cb004c84cf31b31ce3da
Burak <burak.yilmaz@simdatix.com>
2022-07-20 11:13:36 +0300

    add comments on generateId function

--- helpers/generateId.js
+++ helpers/generateId.js
@@ -1,7 +1,9 @@
 const { randomUUID } = require("crypto");
 
+// generate random id
 const generateId = (length) => {
   const generatedUUID = randomUUID().toUpperCase();
+
   if (length < generatedUUID.length) {
     const id = generatedUUID.substring(0, length);
     return id;
